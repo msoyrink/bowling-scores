90faa9d3ef4dbe53f76d4d619fb2bef5fd91f4d1
Söyrinki Mika <soyrimi@ixonos.local>
2019-06-27 12:16:01 +0300

    initial start

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <p>BOWLING</p>
+        </div>
+    );
+}
+
+export default App;
