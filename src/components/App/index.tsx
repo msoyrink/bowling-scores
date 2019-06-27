7fa8839b7936ac302bd027a6d1bc913058095747
Söyrinki Mika <soyrimi@ixonos.local>
2019-06-27 13:45:53 +0300

    material-ui added

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import './styles.css'
+import HomePage from '../HomePage'
+import Login from '../Login'
+import Register from '../Register'
+import Dashboard from '../Dashboard'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline, CircularProgress } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+const theme = createMuiTheme()
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <p>BOWLING</p>
-        </div>
-    );
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/dashboard" component={Dashboard} />
+                </Switch>
+            </Router>
+        </MuiThemeProvider>
+    )
 }
 
 export default App;
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage: React.FC = () => {
+    return (
+        <div>
+            HomePage
+        </div>
+    )
+}
+
+export default Homepage;
